4646bae35c80d068aeea5ac046c329b2563500dc
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-03 20:55:19 -0300

    Instalando componentes 2

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste </Text>
+      <Text> Sla </Text>
       <StatusBar style="auto" />
     </View>
   );

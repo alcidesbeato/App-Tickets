b384f321fc1e5d94ed4e71fa9a5854142deeab35
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-03 21:57:08 -0300

    Mudando padrao arquivo nativo

--- app/App.tsx
+++ app/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Sla thyjgfjhgf </Text>
+      <Text> Sla </Text>
       <StatusBar style="auto" />
     </View>
   );

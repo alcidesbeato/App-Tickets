a8a4f0f1e3b2c84da58a0b4ab16f4db286e10382
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-04 15:29:44 -0300

    Layout da pagina 1

---
+++ app/src/components/CategoryList/data.ts
@@ -0,0 +1,16 @@
+import a from "*.png";
+import b from "*.png";
+import c from "*.png";
+import d from "*.png";
+import e from "*.png";
+import f from "*.jpeg";
+
+
+export default [
+  { name: '1', source: a },
+  { name: '2', source: b },
+  { name: '3', source: c },
+  { name: '4', source: d },
+  { name: '5', source: e },
+  { name: '6', source: f },
+];

a626b63d40a20d3c817fdda368bc82963fe966d6
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-03 22:51:47 -0300

    sla

--- app/src/pages/ComingSoon/styles.ts
+++ app/src/pages/ComingSoon/styles.ts
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
 
-export const Heading = styled.Text ``;
+export const Wrapper = styled(SafeAreaView)`
+  background: blue;
+  flex: 1;
+  padding-top: ${statusBarHeight + 'px'};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Heading = styled(Text)`
+  color: blue;
+  font-family: roboto_500;
+  font-size: 30px;
+`;

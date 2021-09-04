80482e248c9c92969ac664566965ed12400d511d
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-03 21:40:19 -0300

    a

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Sla </Text>
+      <Text> Sla thyjgfjhgf </Text>
       <StatusBar style="auto" />
     </View>
   );

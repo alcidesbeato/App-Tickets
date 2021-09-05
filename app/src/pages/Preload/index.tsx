3d1fcb3c23a495cc7ad1d3cd680ef671d8b9cfea
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2021-09-05 17:30:02 -0300

    corrigindo erros

--- app/src/pages/Preload/index.tsx
+++ app/src/pages/Preload/index.tsx
@@ -2,30 +2,8 @@ import React,{useContext, useEffect} from "react";
 import { Wrapper,  Heading, LoadingIcon } from "./styles";
 import {  Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 
 const Preload: React.FC = () => {
-
-    const navigation = useNavigation();
-
-    useEffect(()=>{
-        const checkToken = async () => {
-            const token = await AsyncStorage.getItem('token');
-            if (token){
-                
-            }//if
-
-            else {
-                navigation.navigate('Inicio');
-
-            }//else
-
-        }//checktoken
-
-        checkToken();
-    }, []);
-   
     return(
         <Wrapper>
 
@@ -41,9 +19,6 @@ const Preload: React.FC = () => {
 
         </Wrapper>
     );
-
-    
-
 
 };
 export default Preload;
